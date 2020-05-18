0a12d8de0347c8542bfade226e7f3bbcfd4bb571
demkantor <demkantor@gmail.com>
2020-05-18 18:11:35 -0500

    added image upload field

--- public/app.js
+++ public/app.js
@@ -6,19 +6,29 @@ document.addEventListener("DOMContentLoaded", event => {
 
     const db =firebase.firestore();
 
-    const myPost = db.collection('post').doc(SW1yfY5nX5YkppaIwOGS)
+    const myPost = db.collection('post').doc('SW1yfY5nX5YkppaIwOGS');
 
-    myPost.get()
-        .then(doc => {
-            const data = doc.data();
-            document.write(data.title + '<br>')
-            document.write(data.createdAt)
-        })
+    myPost.onSnapshot(doc => {
 
+        const data = doc.data();
+        document.write(data.title + '<br>')
+        document.write(data.createdAt + '<br>')
+        document.querySelector('#title').innerHTML = data.title
+    })
     
 
+    const productsRef = db.collection('products');
 
+    const query  = productsRef.where('price', '>=', 5).orderBy('price', 'desc').limit(4)
 
+    query.onSnapshot(products => {
+
+    products.forEach(doc => {
+        const data = doc.data();
+        document.write(`${data.name} at $${data.price}`)
+    })
+
+});
 
 
 });
@@ -36,4 +46,31 @@ function googleLogin() {
         .catch((err) => {
             console.log(err)
         })
+}
+
+
+function updatePost(event) {
+    const db= firebase.firestore();
+    const myPost = db.collection('post').doc('SW1yfY5nX5YkppaIwOGS');
+    myPost.update({ title: event.target.value })
+}
+
+function uploadFile(files) {
+    const storageRef = firebase.storage().ref();
+    const imgRef = storageRef.child('horse.jpg');
+
+    const file = files.item(0);
+
+    const task = imgRef.put(file)
+
+    // successful upload
+    task.then(snapshot => {
+        const url = snapshot.downloadURL
+    })
+
+    // monitor progress
+    task.on('state_changed', snapshot => {
+        console.log(snapshot)
+
+    })
 }
